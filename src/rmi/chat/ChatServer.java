2c5f7ccb153a197ed72ed9fa1e739e50d5649d1e
Joseph-TechX <emmanuelkalujoseph6@gmail.com>
2025-11-03 20:19:10 +0000

    light corrections

--- src/rmi/chat/ChatServer.java
+++ src/rmi/chat/ChatServer.java
@@ -14,16 +14,13 @@ import java.util.List;
  */
 public interface ChatServer extends Remote {
     
-    // Enregistrer un client (username doit être unique)
+    
     void register(String username, ChatClient client) throws RemoteException;
-
-    // Désenregistrer (déconnexion)
+    
     void unregister(String username) throws RemoteException;
-
-    // Envoyer un message (broadcast)
+    
     void sendMessage(String from, String message) throws RemoteException;
 
-    // Lister utilisateurs connectés (utile pour UI)
     List<String> listUsers() throws RemoteException;
     
 }
