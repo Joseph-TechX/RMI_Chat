59f269dba39e41d64b76e108c0e9e59a04cecd73
Joseph-TechX <emmanuelkalujoseph6@gmail.com>
2025-11-03 20:17:02 +0000

    light corrections

--- src/rmi/chat/ChatClient.java
+++ src/rmi/chat/ChatClient.java
@@ -15,29 +15,26 @@ public class ChatClient extends UnicastRemoteObject {
     private final String username;
     private final ChatServer server;
 
-    // Constructeur : exporte le client et le connecte au serveur
     
     public ChatClient(String username, ChatServer server) throws RemoteException {
-        super(); // exportation automatique de l'objet
+        super(); 
         this.username = username;
         this.server = server;
 
-        // Enregistre ce client auprès du serveur
         server.register(username, this);
         System.out.println("[" + username + "] connecté au serveur.");
     }
 
-    // Méthode appelée par le serveur pour recevoir un message
+
     public void receiveMessage(String from, String message) throws RemoteException {
         System.out.println("[" + username + "] ← " + from + " : " + message);
     }
 
-    // Envoyer un message au serveur
+    
     public void send(String message) throws RemoteException {
         server.sendMessage(username, message);
     }
 
-    // Déconnexion propre
     public void disconnect() throws RemoteException {
         server.unregister(username);
         System.out.println("[" + username + "] déconnecté.");
